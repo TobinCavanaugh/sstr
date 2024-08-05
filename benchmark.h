052bbb62ffa5d5a8a2a832f2fc3293565276f85a
Jup1t3rTobinC <tobincavanaugh@gmail.com>
2024-08-05 11:06:32 -0700

    Starting benchmarks

---
+++ benchmark.h
@@ -0,0 +1,84 @@
+//
+// Created by tobin on 2024-08-04.
+//
+
+#ifndef  SSTR_BENCHMARK_H
+#define  SSTRBENCHMARK_H
+
+#include "sstr.h"
+#include "sw.h"
+
+#define BENCHMARK_ITERATIONS 000
+//#define BENCHMARK_ITERATIONS 1
+
+void bench_$append() {
+    char *base = "Object-oriented programming is an exceptionally bad idea which could only have originated in California.";
+    char *app = " - Edsger Dijkstra";
+
+
+    /*sstr.h benchmark*/
+    {
+        double sstr_us = sw_start_us();
+
+        int i = 0;
+        for (; i < BENCHMARK_ITERATIONS; i++) {
+            $begin(__)
+                $ a = base;
+                a = $append(base, app);
+//                printf("(%s)", a);
+            $end(__)
+        }
+
+        sstr_us = sw_stop_us(sstr_us);
+        printf("$append @ %d iterations: %fus\n", BENCHMARK_ITERATIONS, sstr_us);
+    }
+
+    /*C heap benchmark*/
+    {
+        double c_us = sw_start_us();
+
+        int i = 0;
+        for (; i < BENCHMARK_ITERATIONS; i++) {
+            int baseLen = strlen(base);
+            int appLen = strlen(app);
+
+            char *s = malloc(baseLen + 1);
+            strcpy(s, base);
+
+            s = realloc(s, baseLen + appLen + 1);
+            strcpy(s + baseLen, app);
+
+//            printf("(%s)", s);
+
+            free(s);
+        }
+
+
+        c_us = sw_stop_us(c_us);
+        printf("heap @ %d iterations: %fus\n", BENCHMARK_ITERATIONS, c_us);
+    }
+
+    /*C fixed buffer benchmark*/
+    {
+        double cs_us = sw_start_us();
+
+        int i = 0;
+        for (; i < BENCHMARK_ITERATIONS; i++) {
+            char s[128];
+            strcpy(s, base);
+            strcpy(s + strlen(base), app);
+//            printf("(%s)", s);
+        }
+
+        cs_us = sw_stop_us(cs_us);
+        printf("array @ %d iterations: %fus\n", BENCHMARK_ITERATIONS, cs_us);
+    }
+}
+
+
+void bench_all() {
+    bench_$append();
+}
+
+
+#endif //SSTR_BENCHMARK_H
--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include "sstr.h"
 #include "test.h"
+#include "benchmark.h"
 
 int main() {
 
 
     test_all();
+    printf("---\n");
+    bench_all();
 
     return 0;
 }
